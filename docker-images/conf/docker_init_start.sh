a961a9d10def19cde15a28bd83a0df491366fec7
sunzhengfang <sunzhengfang@actionsky.com>
2019-01-11 11:25:31 +0800

    templete commit

--- docker-images/conf/docker_init_start.sh
+++ docker-images/conf/docker_init_start.sh
@@ -4,7 +4,7 @@ echo "dble init&start in docker"
 
 sh /opt/dble/bin/dble start
 sleep 20
-mysql -P9066 -u man1 -h 127.0.0.1 -p654321 -c "create database @@dataNode ='dn1,dn2,dn3,dn4'"
-mysql -P9066 -u root -h 127.0.0.1 -p123456 -c "source /opt/dble/conf/testdb.sql"
+mysql -P9066 -u man1 -h 127.0.0.1 -p654321 -e "create database @@dataNode ='dn1,dn2,dn3,dn4'"
+mysql -P8066 -u root -h 127.0.0.1 -p123456 -e "source /opt/dble/conf/testdb.sql"
 
 echo "dble init finish"
